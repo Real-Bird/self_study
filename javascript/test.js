c3c1473523cc12d90110bdd39fe787c2930a9dd9
RealBird <marbil815@gmail.com>
2022-02-20 19:43:34 +0900

    22-02-20 forEach

--- javascript/test.js
+++ javascript/test.js
@@ -1,18 +1,25 @@
-const nonClick = document.getElementsByClassName("non-click");
+// const nonClick = document.getElementsByClassName("non-click");
+const nonClick = document.querySelectorAll(".non-click");
 
 // div 클릭 시 해당 영역 외 기본 색 참고
 // https://liufeier.tistory.com/22
 
 function handleClick(event) {
-  for (let i = 0; i < nonClick.length; i++) {
-    nonClick[i].classList.remove("click");
-    // div에서 모든 "click" 클래스 제거
-  }
+  // for (let i = 0; i < nonClick.length; i++) {
+  //   nonClick[i].classList.remove("click");
+  //   // div에서 모든 "click" 클래스 제거
+  // }
+  nonClick.forEach((e) => {
+    e.classList.remove("click");
+  })
   event.target.classList.add("click");
   // 클릭한 div만 "click"클래스 추가
 }
 
-for (let i = 0; i < nonClick.length; i++) {
-  nonClick[i].addEventListener("click", handleClick);
-}
+// for (let i = 0; i < nonClick.length; i++) {
+//   nonClick[i].addEventListener("click", handleClick);
+// }
 
+nonClick.forEach((e) => {
+  e.addEventListener("click", handleClick);
+})
