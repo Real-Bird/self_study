3a25d1e0dc01bfa90c89098006b03c44873a66a7
RealBird <marbil815@gmail.com>
2022-02-24 22:21:48 +0900

    22-02-24

--- JS_practice/calendar.js
+++ JS_practice/calendar.js
@@ -54,7 +54,7 @@ function renderingDate() {
     const condition = i >= firstDateIndex && i < lastDateIndex + 1
       ? 'this'
       : 'other';
-    dates[i] = `<div class="day ${condition}">${date}</div>`;
+    dates[i] = `<div class="day ${condition} non-click">${date}</div>`;
   })
 
   // 날짜 추가
@@ -103,3 +103,28 @@ function goToday() {
 preBtn.addEventListener("click", preMonth);
 curBtn.addEventListener("click", goToday);
 nextBtn.addEventListener("click", nextMonth);
+
+const nonClick = document.querySelectorAll(".non-click");
+
+// div 클릭 시 해당 영역 외 기본 색 참고
+// https://liufeier.tistory.com/22
+
+function handleClick(event) {
+  // for (let i = 0; i < nonClick.length; i++) {
+  //   nonClick[i].classList.remove("click");
+  //   // div에서 모든 "click" 클래스 제거
+  // }
+  nonClick.forEach((e) => {
+    e.classList.remove("click");
+  })
+  event.target.classList.add("click");
+  // 클릭한 div만 "click"클래스 추가
+}
+
+// for (let i = 0; i < nonClick.length; i++) {
+//   nonClick[i].addEventListener("click", handleClick);
+// }
+
+nonClick.forEach((e) => {
+  e.addEventListener("click", handleClick);
+})
