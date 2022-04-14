f7aacf01afb295cb5a85f622d1596ebdd6f1e390
Real-Bird <marbil815@gmail.com>
2022-04-14 22:20:22 +0900

    22-04-14 Carousel slider

--- web_study/practiceWeb/practice.js
+++ web_study/practiceWeb/practice.js
@@ -1,33 +1,18 @@
-const img_container = document.querySelector(".container");
-const img_box__btn = document.querySelectorAll(".img_box__btn");
-const img_box = document.querySelectorAll(".img_box");
-let currentIndex = 0;
-const slideCnt = img_box.length;
-
-const prev_btn = document.querySelector(".prev_btn");
-const next_btn = document.querySelector(".next_btn");
-
-next_btn.addEventListener("click", () => {
-  if (currentIndex >= slideCnt - 1) {
-    img_box.forEach((item) => (item.style.transform = "translateX(0%)"));
-    currentIndex = -1;
-  }
-  img_box[(currentIndex + 1) % slideCnt].style.transform = `translateX(-${
-    100 * (currentIndex + 1)
-  }%)`;
-  currentIndex += 1;
-});
-
-prev_btn.addEventListener("click", () => {
-  if (currentIndex <= 0) {
-    currentIndex = slideCnt - 1;
-    img_box[currentIndex].style.transform = `translateX(-${
-      100 * currentIndex
-    }%)`;
+const SHOWING_CLASS = "showing";
+const firstSlide = document.querySelector(".slider__item:first-child");
+function slide() {
+  const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+  if (currentSlide) {
+    currentSlide.classList.remove(SHOWING_CLASS);
+    const nextSlide = currentSlide.nextElementSibling;
+    if (nextSlide) {
+      nextSlide.classList.add(SHOWING_CLASS);
+    } else {
+      firstSlide.classList.add(SHOWING_CLASS);
+    }
   } else {
-    img_box[(currentIndex + 1) % slideCnt].style.transform = `translateX(-${
-      100 * (currentIndex + 1)
-    }%)`;
+    firstSlide.classList.add(SHOWING_CLASS);
   }
-  currentIndex -= 1;
-});
+}
+
+setInterval(slide, 1000);
