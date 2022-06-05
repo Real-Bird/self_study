df0b02a372aafb635902e5b3ae5321e8bc840418
Real-Bird <marbil815@gmail.com>
2022-06-05 20:54:47 +0900

    22-06-05 cote boj Bronze2 1EA

---
+++ Coding_test/2022/06/05/boj_1159.py
@@ -0,0 +1,14 @@
+n = int(input())
+first = {chr(i):0 for i in range(97, 123)}
+
+for i in range(n):
+    first[input()[0]] += 1
+answer = ""
+for key, val in first.items():
+    if val >= 5:
+        answer += key
+
+if answer:
+    print(answer)
+else:
+    print("PREDAJA")
