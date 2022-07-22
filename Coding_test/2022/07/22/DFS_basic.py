5f8a9db969ae9102b749c239e28b9113b73933de
Real-Bird <marbil815@gmail.com>
2022-07-22 21:50:52 +0900

    22-07-22 dfs, bfs, leet 2EA

---
+++ Coding_test/2022/07/22/DFS_basic.py
@@ -0,0 +1,35 @@
+graph = {
+    1: [2, 3, 4],
+    2: [5],
+    3: [5],
+    4: [],
+    5: [6, 7],
+    6: [],
+    7: [3],
+}
+
+
+def recursive_dfs(v, discovered=[]):
+    discovered.append(v)
+    for w in graph[v]:
+        if w not in discovered:
+            discovered = recursive_dfs(w, discovered)
+    return discovered
+
+
+print(f'recursive dfs: {recursive_dfs(1)}')
+
+
+def iterative_dfs(start_v):
+    discoverd = []
+    stack = [start_v]
+    while stack:
+        v = stack.pop()
+        if v not in discoverd:
+            discoverd.append(v)
+            for w in graph[v]:
+                stack.append(w)
+    return discoverd
+
+
+print(f'iterative dfs: {iterative_dfs(1)}')
