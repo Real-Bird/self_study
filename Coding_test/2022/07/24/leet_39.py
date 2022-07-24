def101ecccc54d90485cc8e3e5b84c4e5b77694d
Real-Bird <marbil815@gmail.com>
2022-07-24 22:06:17 +0900

    22-07-24 leet 3EA

---
+++ Coding_test/2022/07/24/leet_39.py
@@ -0,0 +1,16 @@
+def combinationSum(candidates, target):
+    result = []
+
+    def dfs(csum, index, path):
+        if csum < 0:
+            return
+        if csum == 0:
+            result.append(path)
+            return
+        for i in range(index, len(candidates)):
+            dfs(csum - candidates[i], i, path + [candidates[i]])
+    dfs(target, 0, [])
+    return result
+
+
+print(combinationSum([2, 3, 6, 7], 7))
