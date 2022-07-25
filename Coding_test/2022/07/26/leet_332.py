3123c101fff90edc4b30f1e514cca2b17327bc17
Real-Bird <marbil815@gmail.com>
2022-07-26 02:16:09 +0900

    22-07-26 leet 1EA

---
+++ Coding_test/2022/07/26/leet_332.py
@@ -0,0 +1,17 @@
+import collections
+
+
+def findItinerary(tickets):
+    graph = collections.defaultdict(list)
+    for a, b in sorted(tickets, reverse=True):
+        graph[a].append(b)
+
+    route = []
+    def dfs(a):
+        while graph[a]:
+            dfs(graph[a].pop())
+        route.append(a)
+    dfs('JFK')
+    return route[::-1]
+
+print(findItinerary([["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]))
